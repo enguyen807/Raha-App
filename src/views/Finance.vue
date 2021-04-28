e6b22c70ef2926cd5b91a573d0cb0c12cbee7067
Miles Le <eric.nguyen@fleishman.com>
2021-04-28 14:22:55 -0500

    Finish layout for finance page

--- src/views/Finance.vue
+++ src/views/Finance.vue
@@ -1,11 +1,65 @@
 <template>
-  <div class="finance">
-    <h1>This is an finance page</h1>
-  </div>
+  <v-col>
+    <v-tabs v-model="selectedTab" fixed-tabs>
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab
+        class="primary--text"
+        v-for="(tab, i) in tabs"
+        :key="`tabs-${i}`"
+        :href="`#tabs-${i}`"
+      >
+        {{ tab.title }}
+      </v-tab>
+    </v-tabs>
+
+    <form>
+      <v-tabs-items v-model="selectedTab">
+        <v-tab-item value="tabs-0">
+          <v-card flat>
+            <v-card-text>In Add Funds</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item value="tabs-1">
+          <v-card flat>
+            <v-card-text>In Send Funds</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item value="tabs-2">
+          <v-card flat>
+            <v-card-text>In Remove Funds</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </form>
+  </v-col>
 </template>
 
 <script>
 export default {
   name: "Finance",
+  components: {},
+  data: () => ({
+    selectedTab: null,
+    tabs: [
+      {
+        title: "Add Funds",
+        icon: "",
+      },
+      {
+        title: "Send Funds",
+        icon: "",
+      },
+      {
+        title: "Remove Funds",
+        icon: "",
+      },
+    ],
+  }),
+  methods: {
+    handleTabSelection(value) {
+      console.log(value);
+      this.selectedTab = value;
+    },
+  },
 };
 </script>
