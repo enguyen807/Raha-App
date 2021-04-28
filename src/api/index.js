d22948bdc2fa73d5b84b256c6abaa532bd13681c
Miles Le <eric.nguyen@fleishman.com>
2021-04-28 08:11:46 -0500

    Finish up create user component

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,21 @@
 // eslint-disable-next-line prettier/prettier
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.VUE_APP_BASEURL;
-axios.defaults.headers.common["Authorization"] = process.env.VUE_APP_AUTH_TOKEN;
-axios.defaults.headers.post["Content-Type"] = "application/json";
+// axios.defaults.baseURL = process.env.VUE_APP_BASEURL;
+// axios.defaults.headers.common["Authorization"] = process.env.VUE_APP_AUTH_TOKEN;
+// axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export default class Request {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: process.env.VUE_APP_BASEURL,
+      headers: {
+        Authorization: `Basic ${process.env.VUE_APP_AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   async make(method, url, data) {
     const response = await this.instance.request({
       method,
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,64 +1,104 @@
 <template>
   <v-col>
-    <v-row>
-      <v-col cols="4">
-        <v-card class="pa-2" outlined tile>
+    <v-layout row>
+      <v-col cols="12" md="4">
+        <v-card class="pa-5" outlined tile>
           <h2>Create Account</h2>
           <v-spacer></v-spacer>
-          <v-flex>
-            <v-text-field
-              v-model="username"
-              label="Username"
-              :rules="rules"
-              :success-messages="
-                isUsernameAvailable ? checkIfUsernameIsAvailable() : null
-              "
-              :error-messages="
-                isUsernameAvailable ? null : checkIfUsernameIsAvailable()
-              "
-            ></v-text-field>
-            <v-btn
-              class="mt-2"
-              light
-              color="accent"
-              elevation="2"
-              @click="toggle"
-              >Check Username</v-btn
-            >
-          </v-flex>
+          <v-row align-md="baseline" no-gutters>
+            <v-col cols="12" lg="7">
+              <v-text-field
+                v-model="username"
+                label="Username"
+                :rules="rules"
+                :success="success"
+                :success-messages="successMsg"
+                :error="error"
+                :error-messages="errorMsg"
+                @input="handleInputChanges"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" lg="3">
+              <v-btn
+                block
+                small
+                light
+                color="info"
+                elevation="2"
+                @click="checkIfUsernameIsAvailable"
+                >Check Username</v-btn
+              >
+            </v-col>
+          </v-row>
+          <v-btn
+            :disabled="username && isUsernameAvailable ? false : true"
+            block
+            small
+            light
+            color="accent"
+            elevation="2"
+            class="mt-lg-0 mt-md-3 mt-3"
+            >Create Account</v-btn
+          >
         </v-card>
       </v-col>
       <v-divider vertical></v-divider>
-      <v-col cols="8">
+      <v-col cols="12" md="8">
         <v-card class="pa-2" outlined tile>
           .col-12 .col-sm-6 .col-md-8
         </v-card>
       </v-col>
-    </v-row>
+    </v-layout>
   </v-col>
 </template>
 
 <script>
+import Request from "../api/index";
+
 export default {
   name: "User",
   data: () => ({
+    req: new Request(),
     username: null,
     isUsernameAvailable: false,
+    success: false,
+    error: false,
     rules: [
       (value) => !!value || "Required.",
       (value) => (value && value.length >= 5) || "Min 5 characters",
     ],
   }),
   methods: {
-    toggle() {
-      this.isUsernameAvailable = !this.isUsernameAvailable;
-    },
-    checkIfUsernameIsAvailable() {
-      if (this.isUsernameAvailable) {
-        return "Username is available.";
-      }
+    async checkIfUsernameIsAvailable() {
+      const data = JSON.stringify({ data: { name: this.username } });
 
-      return "Username isn't available.";
+      const response = await this.req.make(
+        "POST",
+        "/api/v1/accounts/search",
+        data
+      );
+
+      if (response.data.data.length === 0) {
+        this.isUsernameAvailable = true;
+        this.success = true;
+        this.error = false;
+      } else {
+        this.isUsernameAvailable = false;
+        this.success = false;
+        this.error = true;
+      }
+    },
+    handleInputChanges() {
+      this.success = false;
+      this.error = false;
+    },
+  },
+  computed: {
+    successMsg() {
+      return this.success ? ["Username is available."] : [];
+    },
+    errorMsg() {
+      return this.error ? ["Username is taken."] : [];
     },
   },
 };
