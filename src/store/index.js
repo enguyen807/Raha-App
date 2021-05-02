130903c93f51730ae1131edcd7dd04ff3daac351
Miles Le <eric.nguyen@fleishman.com>
2021-05-02 06:56:35 -0500

    Refactor store and edit readme

--- src/store/index.js
+++ src/store/index.js
@@ -8,100 +8,110 @@ import { alert } from "./modules/alert.module";
 
 Vue.use(Vuex);
 
+const state = {
+  req: new Request(),
+  users: [],
+  dataTableHeaders: [
+    {
+      text: "Name",
+      value: "name",
+      sortable: true,
+    },
+    {
+      text: "Balance",
+      value: "balance",
+      sortable: true,
+    },
+    { text: "Actions", value: "actions", sortable: false },
+  ],
+};
+
+export const getters = {
+  getDataTableHeaders: (state) => {
+    return state.dataTableHeaders;
+  },
+};
+
+export const mutations = {
+  SET_USERS(state, users) {
+    state.users = [...users];
+  },
+  ADD_PAYMENT(state, payload) {
+    const index = state.users.findIndex(
+      (user) => user.id === payload.beneficiary_account_id
+    );
+
+    const beneficiary = {
+      ...this.state.user[index],
+    };
+
+    Vue.set(state.users, index, beneficiary);
+  },
+  UPDATE_BALANCES(state, payload) {
+    const remitterIndex = state.users.findIndex((u) => {
+      return u.id === payload.remitter_account_id;
+    });
+
+    const beneficiaryIndex = state.users.findIndex((u) => {
+      return u.id === payload.beneficiary_account_id;
+    });
+
+    const remitter = {
+      ...this.state.user[remitterIndex],
+    };
+
+    const beneficiary = {
+      ...this.state.user[beneficiaryIndex],
+    };
+
+    remitter.balance -= payload.amount;
+    beneficiary.balance += payload.amount;
+
+    Vue.set(state.users, remitterIndex, remitter);
+    Vue.set(state.users, beneficiaryIndex, beneficiary);
+  },
+  DEDUCT_PAYMENT(state, payload) {
+    const index = state.users.findIndex(
+      (user) => user.id === payload.remitter_account_id
+    );
+
+    const remitter = {
+      ...this.state.user[index],
+    };
+
+    remitter.balance -= payload.amount;
+
+    Vue.set(state.users, index, remitter);
+  },
+};
+
+export const actions = {
+  async getUsers({ dispatch, commit }) {
+    try {
+      const response = await this.state.req.make(
+        "POST",
+        "/api/v1/accounts/search",
+        {}
+      );
+      commit("SET_USERS", response.data.data);
+    } catch (e) {
+      setTimeout(() => {
+        dispatch("alert/error", e.response.statusText, { root: true });
+      });
+    }
+  },
+};
+
+const modules = {
+  user,
+  finance,
+  alert,
+};
+
 export default new Vuex.Store({
-  state: {
-    req: new Request(),
-    users: [],
-    dataTableHeaders: [
-      {
-        text: "Name",
-        value: "name",
-        sortable: true,
-      },
-      {
-        text: "Balance",
-        value: "balance",
-        sortable: true,
-      },
-      { text: "Actions", value: "actions", sortable: false },
-    ],
-  },
-  getters: {
-    getDataTableHeaders: (state) => {
-      return state.dataTableHeaders;
-    },
-  },
-  mutations: {
-    SET_USERS(state, users) {
-      state.users = [...users];
-    },
-    ADD_PAYMENT(state, payload) {
-      const index = state.users.findIndex(
-        (user) => user.id === payload.beneficiary_account_id
-      );
-
-      const beneficiary = {
-        ...this.state.user[index],
-      };
-
-      Vue.set(state.users, index, beneficiary);
-    },
-    UPDATE_BALANCES(state, payload) {
-      const remitterIndex = state.users.findIndex((u) => {
-        return u.id === payload.remitter_account_id;
-      });
-
-      const beneficiaryIndex = state.users.findIndex((u) => {
-        return u.id === payload.beneficiary_account_id;
-      });
-
-      const remitter = {
-        ...this.state.user[remitterIndex],
-      };
-
-      const beneficiary = {
-        ...this.state.user[beneficiaryIndex],
-      };
-
-      remitter.balance -= payload.amount;
-      beneficiary.balance += payload.amount;
-
-      Vue.set(state.users, remitterIndex, remitter);
-      Vue.set(state.users, beneficiaryIndex, beneficiary);
-    },
-    DEDUCT_PAYMENT(state, payload) {
-      const index = state.users.findIndex(
-        (user) => user.id === payload.remitter_account_id
-      );
-
-      const remitter = {
-        ...this.state.user[index],
-      };
-
-      remitter.balance -= payload.amount;
-
-      Vue.set(state.users, index, remitter);
-    },
-  },
-  actions: {
-    async getUsers({ dispatch, commit }) {
-      try {
-        const response = await this.state.req.make(
-          "POST",
-          "/api/v1/accounts/search",
-          {}
-        );
-        commit("SET_USERS", response.data.data);
-      } catch (e) {
-        setTimeout(() => {
-          dispatch("alert/error", e.response.statusText, { root: true });
-        });
-      }
-    },
-  },
-  modules: {
-    user,
-    finance,
-    alert,
-  },
+  state,
+  getters,
+  mutations,
+  actions,
+  modules,
 });
